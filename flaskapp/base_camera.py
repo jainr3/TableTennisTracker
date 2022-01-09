9c8a7e96d85e017ca7b2ab384be4f30c34358595
jainr3 <Rahul96Jain@gmail.com>
2022-01-09 12:43:58 -0500

    webcam bugfix - 20 sec timeout

--- flaskapp/base_camera.py
+++ flaskapp/base_camera.py
@@ -94,8 +94,8 @@ class BaseCamera(object):
             time.sleep(0)
 
             # if there hasn't been any clients asking for frames in
-            # the last 10 seconds then stop the thread
-            if time.time() - BaseCamera.last_access > 10:
+            # the last 20 seconds then stop the thread
+            if time.time() - BaseCamera.last_access > 20:
                 frames_iterator.close()
                 print('Stopping camera thread due to inactivity.')
                 break
